83a3dde59163a8ed638eb6828521038c55d4e524
Yaoming Jiang <yao4ming@gmail.com>
2015-04-11 01:27:40 -0400

    Crint

--- src/os.java
+++ src/os.java
@@ -1,10 +1,40 @@
 
 public class os {
 
+    static int count = 0;
+    static Job[] jobTable;
+    static final int MAXJOBS = 50;
+    static int[] interruptedJob;
+    static float[] memoryTable;
 
     public static void startup(){
-        //init
 
+        //allocate memory for JobTable
+        jobTable = new Job[MAXJOBS];
+        for (int i = 0; i < MAXJOBS; i++) {
+            jobTable[i] = new Job();
+        }
+
+        //allocate memory for interrupted jobs
+        interruptedJob = new int[6];
+
+        //allocate memory for free space table
+        memoryTable = new float[100];
+
+
+    }
+
+    //encapsulate Job inside OS as member class
+    public static class Job {
+        int jobNum;
+        int priority;
+        int jobSize;
+        int cpuTime;
+        int currentTime;
+    }
+
+    public static void save(int[] p){
+        System.arraycopy(p, 0, interruptedJob, 0, p.length);
     }
 
 
@@ -14,14 +44,22 @@ public class os {
     public static void Crint (int[] a, int []p){
 
         //store state of interrupted job
+        save(p);
 
-        //store job info in job table
+        jobTable[count].jobNum = p[1];
+        jobTable[count].priority = p[2];
+        jobTable[count].jobSize = p[3];
+        jobTable[count].cpuTime = p[4];
+        jobTable[count].currentTime = p[5];
+        count++;
 
         //long-term scheduler(Decide which job to swap into memory)
 
         //swapper(swap job into memory)
+        sos.siodrum(p[1], p[3], 98, 0);
 
         //set action and params for SOS
+        a[0] = 1;
 
     }
 
@@ -32,7 +70,7 @@ public class os {
 
     //finished swapping job
     public static void Drmint (int[] a, int []p){
-
+        System.out.println("-----------------------------------------------------------------------------------------------");
     }
 
     //job ran out of time
